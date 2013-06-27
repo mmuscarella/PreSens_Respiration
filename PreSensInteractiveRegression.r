89afc16d83c7b2e8fdc92454a28c3c3af383fd61
Mario Muscarella <mmuscare@indiana.edu>
2013-06-27 11:58:48 -0400

    Please enter the commit message for your changes. Lines starting

--- PreSensInteractiveRegression.r
+++ PreSensInteractiveRegression.r
@@ -1,28 +1,23 @@
 ### Analysis of PreSens Respiration Data
 ### Written By: Mario Muscarella
-### Last Update: 20 June 2013
+### Last Update: 26 June 2013
 
 ## This analysis uses the R package - rpanel which does interactive regression
 ## The goal is to use the interactive view to pick the area to analyze
 ## rpanel - Interactive Regression 
 ## Multiple trend chart version; Annual anomaly data
 
-setwd("C:/Users/Mario Muscarella/Documents/PhD Files/PreSens/Analysis Script/")
-rm(list=ls())
 require("rpanel")||install.packages("rpanel")
 require("gWidgets")||install.packages("gWidgets")
 require("xlsx")||install.packages("xlsx")
-#require("gWidgetsRGtk2")||install.packages("gWidgetsRGtk2")
-#require("RGtk2")||install.packages("RGtk2")
-#require("lattice")||install.packages("lattice")
-#require("grid")||install.packages("grid")
-#require("cairoDevice")||install.packages("cairoDevice")
-#source("PreSensInteractiveRegressionFunctions.r")
 
 #options("guiToolkit"="RGtk2")     
 options(digits=6)
 script <- "Mario's Bad Ass M-F'ing Interactive Regression Analysis"
-user.name <- "M. Muscarella"
+if("user.name" %in% ls() == FALSE){
+  user.name <- "Nobody"
+  } else {
+  }
 
 ## Import Data #################################################################
 infile <- gfile("Open PreSens Data...",type="open")
@@ -62,10 +57,11 @@ draw <- function(panel) {
     end <- max(Time)}
                                                             
   data.in$samp <- panel$samp
+  name <- panel$sample.name
   
 # Text placement points    
   xaxis_pt <- min(Time) + 0.25*(max(Time)-min(Time)) 
-  yaxis_pt <- 200
+  yaxis_pt <- 220
   
 # Make data subset based on start & end yrs
   sub <- subset(data.in, Time >= start)
@@ -94,10 +90,11 @@ draw <- function(panel) {
   y_vals = c(a+b*panel$start, a+b*panel$end)
 	lines(x_vals, y_vals, col= "red")
   points(sub$Time, sub$samp, pch = 19, col = "red", type = "p")
-  text(xaxis_pt, yaxis_pt, paste("Period: ", start.2, " to " , end.2, "Hrs"))  
-  text(xaxis_pt, yaxis_pt - 10, bquote(Rate == .(rate) ~ µM ~ O[2] ~ Hr^-1), cex=1) #expression(paste("Rate = ", rate, "µM O ", "Hr"^"-1")),cex=1)
-  text(xaxis_pt, yaxis_pt - 20, bquote(R^2 == .(r2)), cex=1) #expression(paste("R"^"2"," = " , r2)), cex=1)
-  text(xaxis_pt, yaxis_pt - 30, paste("P-value = ", p), cex=1)
+  text(xaxis_pt, yaxis_pt, paste("Sample: ", name))
+  text(xaxis_pt, yaxis_pt - 10, paste("Period: ", start.2, " to " , end.2, "Hrs"))  
+  text(xaxis_pt, yaxis_pt - 20, bquote(Rate == .(rate) ~ µM ~ O[2] ~ Hr^-1), cex=1)
+  text(xaxis_pt, yaxis_pt - 30, bquote(R^2 == .(r2)), cex=1)
+  text(xaxis_pt, yaxis_pt - 40, paste("P-value = ", p), cex=1)
   
 ### Outer Margin Annotation
 	my_date <- format(Sys.time(), "%m/%d/%y")
@@ -105,19 +102,35 @@ draw <- function(panel) {
   mtext(user.name, side = 1, line = 0.75, cex=0.8, outer=T, adj = 0.75)
 	mtext(my_date, side = 1, line =.75, cex = 0.8, outer = T, adj = 1)
 	data.out <- list(Start=start, End=end, Rate=rate, R2=r2, Pvalue=p)
-	print(data.out)
-	return(data.out)
 	panel
   }
    
 collect.data <- function(panel) {
-  data.sample <- readline("What is the name of this sample?")
-  data.start <- data.out$Start
-  data.end <- data.out$End
-  data.rate <- data.out$Rate
-  data.r2 <- data.out$r2
-  data.p <- data.out$Pvalue
-  data.sample <- panel$samp
+  data.in$samp <- panel$samp
+  name <- panel$sample.name
+  start <- panel$start
+  end <- panel$end
+  # Text placement points    
+  xaxis_pt <- min(Time) + 0.25*(max(Time)-min(Time)) 
+  yaxis_pt <- 200
+  # Make data subset based on start & end yrs
+  sub <- subset(data.in, Time >= start)
+  sub <- subset(sub, Time <= end)
+  # Linear trend line lm & coefs / stats
+  trend <- lm(sub$samp ~ sub$Time)
+  a <- as.numeric(coef(trend)[1]);  b <- as.numeric(coef(trend)[2])
+  r2 <- round(summary(trend)$r.squared, 3)
+  p <- round(anova(trend)$'Pr(>F)'[1], 4)
+  p <- ifelse (p == 0, "<0.001", p)
+  start.2 <- signif(start, digits = 3)
+  end.2 <- signif(end, digits = 3)
+  rate <- signif(-b,3)
+  data.sample <- name
+  data.start <- start.2
+  data.end <- end.2
+  data.rate <- rate
+  data.r2 <- r2
+  data.p <- p
   data.out <- c(data.sample, data.start, data.end, data.rate, data.r2, data.p)
   write.table(as.matrix(t(data.out)), file=outfile, append=T, row.names=F, col.names=F, sep=",", quote=FALSE)
   panel
@@ -131,10 +144,11 @@ end.session <- function(panel) {
                             
 ## rpanel controls - enter start and end yrs for portion of full data set to be used
 rpplot <- rp.control(title="Interactive Regression", start=0, end = max(Time), initval = samples[1])
-rp.listbox(rpplot, variable = samp, vals = "samples", labels = samples, action = draw)
+rp.listbox(rpplot, variable = samp, vals = "Samples", labels = samples, action = draw)
 rp.slider(rpplot, start, action = draw, from = 0, to =  max(Time))
 rp.slider(rpplot, end, action = draw, from = 0, to =  max(Time))
-rp.doublebutton(rpplot, var = start, step = 1, title = "start fine adjustment", action = draw)
-rp.doublebutton(rpplot, var = end, step = 1, title = "end fine adjustment", action = draw)
+rp.doublebutton(rpplot, var = start, step = 1, title = "Start Fine Adjustment", action = draw)
+rp.doublebutton(rpplot, var = end, step = 1, title = "End Fine Adjustment", action = draw)
+rp.textentry(rpplot, var = sample.name, action = draw, labels = "Sample Name", initval = "")
 rp.button(rpplot, title = "save", action = collect.data)
 rp.button(rpplot, title = "quit", action = end.session, quitbutton=T)
---
+++ PreSensTest.r
@@ -0,0 +1,11 @@
+### Test PreSens Analysis
+### Written By: Mario Muscarella
+### Last Update: 26 June 2013
+
+setwd("C:/Users/Mario Muscarella/Documents/PhD Files/PreSens/Analysis Script/")
+rm(list=ls())
+
+user.name <- "M. Muscarella"
+source("PreSensInteractiveRegression.r")
+
+## Make sure to press enter after entering a sample name in order to update info
